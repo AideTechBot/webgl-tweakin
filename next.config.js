26f25a16d296d664d812a0053e89123483dea67a
aidetechbot <mandio0302@gmail.com>
2021-04-22 04:13:54 -0400

    try now

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   basePath: '/webgl-tweakin',
-  routes: [
-    {
-      "src": "/_next/static/(?:[^/]+/pages|chunks|runtime|css|media)/.+",
-      "headers": { "cache-control": "public,max-age=31536000,immutable" },
-      "continue": true
-    },
-    { "handle": "filesystem" },
-    {"src": "/(.*)", "dest": "/$1.html"}
-  ]
+  exportTrailingSlash: true
 }
